1ce8513e6f7d64a20a8abd5c2c323775cffb34b6
mimpie <yesl3715@naver.com>
2022-11-13 05:05:26 +0900

    image add

--- lib/ui/DiagonalPage.dart
+++ lib/ui/DiagonalPage.dart
@@ -6,27 +6,60 @@ class DiagonalPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(children: <Widget>[Positioned(
-        child: ButtonEachTwoRightTriangleWithDivider(
-          bottomLeftTriangleBackgroundColor: Color.fromRGBO(208, 227, 235, 1),
-          dividerColor: Colors.blueAccent,
-          height: double.infinity,
-          width: double.infinity,
-          topRightTriangleDecoration: BoxDecoration(
-            color: Color.fromRGBO(249, 222, 202, 1),
-          ),
-          topRightTriangleOnTap: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => SecondScreen()));
-          },
-          bottomLeftTriangleOnTap: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => SecondScreen()));
-          },
+        body: Stack(children: <Widget>[
+      ButtonEachTwoRightTriangleWithDivider(
+        bottomLeftTriangleBackgroundColor: Color.fromRGBO(208, 227, 235, 1),
+        dividerColor: Colors.blueAccent,
+        height: double.infinity,
+        width: double.infinity,
+        topRightTriangleDecoration: BoxDecoration(
+          color: Color.fromRGBO(249, 222, 202, 1),
+        ),
+        topRightTriangleOnTap: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => SecondScreen()));
+        },
+        bottomLeftTriangleOnTap: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => SecondScreen()));
+        },
+      ),
+      Positioned(
+        left: 40,
+        top: 10,
+        child: Container(
+          height: 200,
+          width: 200,
+          margin: EdgeInsets.all(100),
+          child: GestureDetector(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SecondScreen()));
+              },
+              child: Image.asset('images/female.png')),
         ),
       ),
-          GestureDetector(
-            child: Image.asset('images/mom.png')),
+      //SizedBox(height: 200),
+          Positioned(child:
+            Center(
+              child: Text("아들~ 이면 왼쪽하단을 선택하슈\n딸~ 이면 오른쪽상단을 선택"),
+            ),
+          ),
+      Positioned(
+        right: 30,
+        top: 350,
+        child: Container(
+          height: 200,
+          width: 200,
+          margin: EdgeInsets.all(100),
+          child: GestureDetector(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SecondScreen()));
+              },
+              child: Image.asset('images/male.png')),
+        ),
+      ),
     ]));
   }
 }
